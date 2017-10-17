26a8843cd57ba02a10ba61d624fd558b3fb65cab
Gurmeet Singh <chdgurmeet@gmail.com>
2017-10-17 12:04:47 -0400

    Added for loop withe echo

--- do-uniq-species.sh
+++ do-uniq-species.sh
@@ -1,6 +1,11 @@
-# Purpose: Find the unique species in a dataset 
+ "Unique species in $data_file"
+#Extract species names from data_file
+# Purpose: Find the unique 
 # Date: Tuesday, October 17, 2017
 # Author: Gurmeet Singh
 
 # Loop over files
 for data_file in $@
+ do
+echo "Unique species in $data_file"
+cut -d, -f 2 $data_file
